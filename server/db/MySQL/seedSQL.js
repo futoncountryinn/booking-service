cee17abfe493379a91a684f9d2c7f09d37747f1e
Keenan Thomson <keenanthomson@gmail.com>
2019-08-21 21:08:03 -0400

    Update repo, work on sql seed

--- server/db/MySQL/seedSQL.js
+++ server/db/MySQL/seedSQL.js
@@ -1,33 +1,51 @@
 const mysql = require('mysql');
 const moment = require('moment');
 const Promise = require('bluebird');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
+// const fs = require('fs');
 
-const connection = mysql.createConnection({
-  user: 'root',
-  password: '',
-  database: 'airbnb'
-});
+// const connection = mysql.createConnection({
+//   user: 'root',
+//   password: '',
+//   database: 'airbnb'
+// });
 
-const db = Promise.promisifyAll(connection);
+// const db = Promise.promisifyAll(connection);
 
-const seedDatabase = () => {
-  for (let i = 1; i < 101; i++) {
-    // const now = moment([moment().year(), moment().month(), 1]); // error here: by referring to same moment() dates were identical
-    startDate = moment([moment().year(), moment().month(), 1]).add(i, 'month')
-    endDate = moment([moment().year(), moment().month(), 1]).add(i, 'month').add(Math.floor(Math.random() * 7), 'days');
-    const checkout = {
-      checkin: startDate,
-      checkout: endDate
-    }
-
-    db.queryAsync(`INSERT INTO checkout SET ?`, checkout, (err) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(`Inserted record ${JSON.stringify(checkout)}`);
-    });
+const createReservation = (index, cb) => {
+  startDate = moment([moment().year(), moment().month(), 1]).add(index, 'month')
+  endDate = moment([moment().year(), moment().month(), 1]).add(index, 'month').add(Math.floor(Math.random() * 7), 'days');
+  const checkout = {
+    checkin: startDate,
+    checkout: endDate
   }
+  cb(checkout);
 };
 
-db.connectAsync()
-  .then(() => seedDatabase());
+// below attempts to populate CSV
+
+const csvWriter = createCsvWriter({
+  path: './server/db/MySQL/records.csv',
+  header: [
+    {id: 'checkin', title: 'CheckIn'},
+    {id: 'checkout', title: 'CheckOut'}
+  ],
+  append: true
+});
+
+const saveRecordsToCSV = (totalNewRecords) => {
+  let csvEntries = [];
+  for (let i = 0; i < totalNewRecords; i++) {
+    createReservation(i, (entry) => {
+      csvEntries.push(entry);
+    });
+  };
+  csvWriter.writeRecords(csvEntries)
+    .then(() => {
+      console.log(`New records saved successully.`)
+    });
+};
+
+for (let i = 0; i < 8; i++) {
+  saveRecordsToCSV(1000000);
+}

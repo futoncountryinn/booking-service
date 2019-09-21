7033144d16e3c136d18ffd1107963705bae68af2
Keenan Thomson <keenanthomson@gmail.com>
2019-09-20 22:59:28 -0400

    Update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM node:10.16.0
 RUN mkdir -p /src/app
 WORKDIR /src/app
 COPY . /src/app/
-RUN npm install
+RUN npm install --no-package-lock
 CMD [ "npm", "start" ]
--- k6_get.js
+++ k6_get.js
@@ -3,15 +3,15 @@ import { check, sleep } from "k6";
 
 export let options = {
   rps: 1000,
-  vus: 10,
+  vus: 150,
   duration: "10s"
 };
 
 export default function() {
-  let url = "http://localhost:3002/reservation";
+  let url = "http://load-balancer-1-2135754840.us-east-2.elb.amazonaws.com/reservation";
   let res = http.get(url);
   check(res, {
-    "transaction time OK": (r) => r.timings.duration < 200
+    "transaction time OK": (r) => r.timings.duration < 500
   });
   sleep(1);
 };
